c2cc46e798f8e44794a0d5497b0db09959521497
A2v10 <hey.a2v10@gmail.com>
2024-01-14 22:40:11 +0530

    removed cherryPicked env

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const processEnv = {};
-  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
-
-  return {
-    define: {
-      'process.env': processEnv
-    },
-    plugins: [react()],
-  };
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env': env
+        },
+        plugins: [react()],
+    };
 });
