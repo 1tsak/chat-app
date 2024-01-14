43965abe19d25a6b14f7aa46cf330c290bac097d
A2v10 <hey.a2v10@gmail.com>
2024-01-14 23:23:43 +0530

    Updated Config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-            'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV)
-        },
-        plugins: [react()],
-    };
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process": process,
+      "process.env":env,
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+    },
+    plugins: [react()],
+  };
 });
