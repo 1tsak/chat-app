7a335bd7959c7ec6c006b6a1f2792f266e405430
A2v10 <hey.a2v10@gmail.com>
2024-01-14 22:37:33 +0530

    FixedBugs

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-            'process.env': env
-        },
-        plugins: [react()],
-    };
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+
+  return {
+    define: {
+      'process.env': processEnv
+    },
+    plugins: [react()],
+  };
 });
